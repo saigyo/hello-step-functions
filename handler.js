c5569efd58e6499a62f2c53cfed3631bd09909af
Markus Ackermann <markus.ackermann@europace.de>
2017-05-20 21:44:18 +0200

    Add a hello step-function, demonstrating failure handling.

--- handler.js
+++ handler.js
@@ -1,16 +1,11 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  console.log('Received event:', JSON.stringify(event, null, 2));
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  if (event.fail === true) {
+    callback(new Error("I was asked to fail."))
+  }
+  
+  callback(null, "Hello, " + event.who + "!");
 };
